aa0a0118cb356962f2e44e55ae62ac78215e91e5
Gian <ggianmanzo@gmail.com>
2024-10-10 15:11:16 -0300

    setup view code

--- weather-mate/ViewController.swift
+++ weather-mate/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }// loaded once when the screen appears only
+    
+    
+    private func setupView() {
+        view.backgroundColor = .red
+        setHierarchy()
+        setConstraints()
     }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
 
-
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
+    
 }
 
+
+
+
+
+
+
+
+
+
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }// whenever the screen appears this executes
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//    }// before the screen appears this executes
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//    } // when it will disappear that the screen appears this executes
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//    }// When the screen has disappeared, this appears.
+    
